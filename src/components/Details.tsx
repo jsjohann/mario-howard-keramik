5be9e3d8c5b45e1e5850310fbc37276aacb110e8
Johann Schumacher <info@johannschumacher.de>
2022-12-13 20:21:23 +0100

    change animation

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,79 +1,79 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Container, Row, Col} from 'react-bootstrap';
-import { Link } from "gatsby"
+import { graphql, Link, StaticQuery } from "gatsby"
 import DetailsVideo from "../assets/detail-video.mp4";
-import { StaticImage } from 'gatsby-plugin-image';
+import { GatsbyImage, getImage, StaticImage } from 'gatsby-plugin-image';
 
 const detailsStyle = {
 
 }
 
-const Details = () => {
+const Details = (props) => {
   const frameCount = 119;
 
-  const currentFrame = (index: number) => (
-    `/detail-images/animation-detail${index.toString().padStart(3, '0')}.jpg`
-  )
-
-  const canvas = useRef(null);
-
-  let img = null;
-
-  const updateImage = (frameIndex: number) => {
-    img.src = currentFrame(frameIndex);
-   // drawImage();
-  }
-
-  const drawImage = () => {
-    var scale = Math.max(canvas.current.width / img.width, canvas.current.height / img.height);
-    // get the top left position of the image
-    var x = (canvas.current.width - img.width) / 2;
-    var y = (canvas.current.height - img.height) / 2;
-    canvas.current.getContext('2d').drawImage(img, x, y, img.width * scale, img.height * scale);
-  }
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const container = useRef(null);
 
   const updateImageOnScroll = (html: HTMLElement) => {
     const windowHeight = window.innerHeight;
-    const elementHeight = canvas.current.scrollHeight;
-    const currentPosition = canvas.current.getBoundingClientRect().y + elementHeight;
+    const elementHeight = container.current.scrollHeight;
+    const currentPosition = container.current.getBoundingClientRect().y + elementHeight;
     const delta = windowHeight + elementHeight;
     const relativePosition = currentPosition / delta;
     const fraction = Math.min(Math.max(relativePosition, 0), 1);
     const index = Math.round(fraction * frameCount);
-    requestAnimationFrame(() => updateImage(index))
+    setCurrentIndex(index);
+    // requestAnimationFrame(() => updateImage(index));
   }
   
   useEffect(() => {
     const onScroll = () => updateImageOnScroll(document.documentElement);
-    img = new Image();
-    img.onload = function(){
-      drawImage();
-    }
-    img.src = currentFrame(0);
-    canvas.current.width = canvas.current.clientWidth * window.devicePixelRatio;
-    canvas.current.height = canvas.current.clientHeight * window.devicePixelRatio;
-    // clean up code
+
     window.removeEventListener('scroll', onScroll);
     window.addEventListener('scroll', onScroll, { passive: true });
     return () => window.removeEventListener('scroll', onScroll);
   }, []);
 
   return (
-    <Container fluid="xl" style={detailsStyle} className="p-0 mb-4">
-        <Row>
-          <Col xs={6}>
-            <StaticImage src="../assets/Einblick.jpg" alt="Einblick in die Werkstatt"></StaticImage>
-          </Col>
-          <Col xs={6} className="d-flex flex-column">
-            <div className="mb-4" style={{ flex: 1}}>
-              <canvas style={{ height: '100%', width: '100%' }} ref={canvas}></canvas>
-            </div>
-            <div style={{ flex: 1}}>
-              <StaticImage style={{ height: '100%' }} src="../assets/Detail.jpg" alt="Detailaufnahme bei der Arbeit"></StaticImage>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+    <StaticQuery
+      query={graphql`
+        query HeadingQuery {
+          photos: allFile(
+            filter: {name: {regex: "/animation-detail/"}}, sort: {fields: name}
+          ) {
+          edges {
+            node {
+              absolutePath
+              name
+              childImageSharp {
+                gatsbyImageData(formats: [AUTO, WEBP, AVIF])
+              }
+            }
+          }
+        }
+        }
+      `}
+      render = {
+        (data) => (
+           <Container fluid="xl" style={detailsStyle} className="p-0 mb-4">
+            <Row>
+              <Col xs={6}>
+                <StaticImage src="../assets/Einblick.jpg" alt="Einblick in die Werkstatt"></StaticImage>
+              </Col>
+              <Col xs={6} className="d-flex flex-column">
+                <div className="mb-4" style={{ position: 'relative', flex: 1, overflow: 'hidden', whiteSpace: 'nowrap'}} ref={container}>
+                  {data.photos.edges.map((img, index) => (
+                    <GatsbyImage loading="eager" style={{ position: 'absolute', left: 0, top: 0, width: '100%', height: '100%', opacity: img.node.name === `animation-detail${currentIndex.toString().padStart(3, '0')}` ? 1 : 0, willChange: 'opacity', transform: 'translateZ(0)' }} objectFit={'cover'} image={getImage(img.node)} key={index} alt={`Detailaufnahme bei der Arbeit Bildsequenz Index ${index}`} />
+                  ))}
+                </div>
+                <div style={{ flex: 1}}>
+                  <StaticImage style={{ height: '100%' }} src="../assets/Detail.jpg" alt="Detailaufnahme bei der Arbeit"></StaticImage>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        )}
+      />
   )
 }
 
