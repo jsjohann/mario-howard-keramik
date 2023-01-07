0e8931e02271006667277ee8c41a89a8e2295e2e
Johann Schumacher <info@johannschumacher.de>
2023-01-07 19:05:23 +0100

    fix deployed link

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,10 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  pathPrefix: '/mario-howard-keramik/',
+  pathPrefix: '/',
   siteMetadata: {
-    title: `mario-howard-keramik`,
-    siteUrl: `https://jsjohann.github.io`
+    title: `Mario Howard Keramik`,
+    siteUrl: `https://mario-howard.de`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
