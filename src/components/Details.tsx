85e86ac6e0b9c2ef7f7ebe98e5e685760ce30110
Johann Schumacher <info@johannschumacher.de>
2023-01-14 12:31:55 +0100

    fix scroll animation

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -21,7 +21,7 @@ const Details = (props) => {
     const delta = windowHeight + elementHeight;
     const relativePosition = currentPosition / delta;
     const fraction = Math.min(Math.max(relativePosition, 0), 1);
-    const index = Math.min(Math.round(fraction * frameCount) + 1, frameCount);
+    const index = Math.min(Math.round(fraction * frameCount), frameCount);
     setCurrentIndex(index);
     // requestAnimationFrame(() => updateImage(index));
   }
@@ -63,7 +63,7 @@ const Details = (props) => {
               <Col xs={6} className="d-flex flex-column">
                 <div className="mb-4" style={{ position: 'relative', flex: 1, overflow: 'hidden', whiteSpace: 'nowrap'}} ref={container}>
                   {data.photos.edges.map((img, index) => (
-                    <GatsbyImage loading="eager" style={{ position: 'absolute', left: 0, top: 0, width: '100%', height: '100%', display: index === currentIndex ? 'block' : 'none', willChange: 'display', transform: 'translateZ(0)' }} objectFit={'cover'} image={getImage(img.node)} key={index} alt={`Detailaufnahme bei der Arbeit Bildsequenz Index ${index}`} />
+                    <GatsbyImage loading="eager" style={{ position: 'absolute', left: 0, top: 0, width: '100%', height: '100%', opacity: index === currentIndex ? 1 : 0, willChange: 'opacity', transform: 'translateZ(0)' }} objectFit={'cover'} image={getImage(img.node)} key={index} alt={`Detailaufnahme bei der Arbeit Bildsequenz Index ${index}`} />
                   ))}
                 </div>
                 <div style={{ flex: 1}}>
