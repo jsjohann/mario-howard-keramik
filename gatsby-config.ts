0b27a15e5a7bf3e1d852992efa56006f4f6eaeb2
Johann Schumacher <info@johannschumacher.de>
2023-01-21 11:19:43 +0100

    fix sitemap exclude

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -42,7 +42,7 @@ const config: GatsbyConfig = {
     {
       resolve: 'gatsby-plugin-sitemap',
       options: {
-        exclude: ['/imprint', '/datenschutz'],
+        excludes: ['/imprint', '/datenschutz'],
       },
     }
   ]
